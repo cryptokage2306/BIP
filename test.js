2b23f69ee1a0d4527db3a5851fc27e39544def8f
Vivek Jain <vkjvivek7@gmail.com>
2021-04-09 02:02:50 +0530

    cleanup

--- test.js
+++
@@ -1,6 +0,0 @@
-const createHmac = require("create-hmac");
-function hmacSHA512(key, data) {
-  return createHmac("sha512", key).update(data).digest();
-}
-
-console.log(hmacSHA512(Buffer.from('Bitcoin seed', 'utf8'), '123344343423424234324'))
